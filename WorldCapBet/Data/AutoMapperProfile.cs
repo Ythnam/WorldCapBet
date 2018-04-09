eab9c5b1dd1af35eb449c5bcfbe2287b532f55c5
Ythnam <phildesalondepce@hotmail.fr>
2018-04-09 17:53:21 +0200

    Add Automapper

---
+++ WorldCapBet/Data/AutoMapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCapBet.Model;
+using WorldCapBet.ModelDTO;
+
+namespace WorldCapBet.Data
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<UserDTO, User>();
+
+            CreateMap<Match, MatchDTO>();
+            CreateMap<MatchDTO, Match>();
+
+            CreateMap<Pronostic, PronosticDTO>();
+            CreateMap<PronosticDTO, Pronostic>();
+        }
+        
+    }
+}
