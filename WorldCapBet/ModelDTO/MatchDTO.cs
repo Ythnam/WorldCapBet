c9515bbc16efbd5ed113c56155d4762d499d15c3
Ythnam <phildesalondepce@hotmail.fr>
2018-04-09 17:35:55 +0200

    Ajout des classes DTO

---
+++ WorldCapBet/ModelDTO/MatchDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCapBet.ModelDTO
+{
+    public class MatchDTO
+    {
+        public int Id { get; set; }
+
+        public string Team1 { get; set; }
+
+        public string Team2 { get; set; }
+
+        public int ScoreTeam1 { get; set; }
+
+        public int ScoreTeam2 { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
